25524d7976fdc824773e40ed1b859eaf856c128b
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-09-27 14:05:11 +0200

    animaciones, css

--- pagina/script.js
+++ pagina/script.js
@@ -1,7 +1,20 @@
 // script.js
 document.getElementById("claro").addEventListener("click", modoClaro)
 document.getElementById("oscuro").addEventListener("click", modoOscuro)
-document.getElementById("oscuro").style.backgroundColor = "grey";
+document.getElementById("oscuro").style.backgroundColor = "#00ffcc";
+
+document.getElementById("marcha").addEventListener("click", moverElemento)
+document.getElementById("reset").addEventListener("click", reset)
+
+/*
+   let mensaje = document.createElement('p');
+    mensaje.textContent("Maquina en marcha, espere a que coja el chocolate");
+    let derecha = document.getElementById("barra-derecha");
+    derecha.appendChild(mensaje);
+*/ 
+
+var marcha;
+var tipo;
 
 
 function modoClaro(){
@@ -22,4 +35,45 @@ function modoOscuro(){
     document.getElementById("barra-izquierda").style.backgroundColor = "#333"
     document.getElementById("contenedor").style.backgroundColor = "#222"
     document.body.style.color = "white"
+}
+
+function reset(){
+    location.reload();
+}
+
+function moverElemento() {
+ 
+
+    const chocolate = document.getElementById("chocolate");
+    const posicion = "a24"/* Aquí deberías obtener el valor de mis_datos.posicion */;
+
+    // Obtener el div objetivo
+    const divObjetivo = document.querySelector(`.tabla-chocolates .${posicion}`);
+  
+    const rectChoco = chocolate.getBoundingClientRect();
+    const rectObjetivo = divObjetivo.getBoundingClientRect();
+
+    // Calcular la distancia
+    const distanciaX = rectObjetivo.left + (rectObjetivo.width / 2) - (rectChoco.width / 2) - rectChoco.left;
+    const distanciaY = rectObjetivo.top + (rectObjetivo.height / 2) - (rectChoco.height / 2) - rectChoco.top;
+
+    // Mover el chocolate al centro del objetivo
+    chocolate.style.transform = `translate(${distanciaX}px, ${distanciaY}px)`;
+
+    //cambiar color fondo, 1 blanco 0 negro
+
+    tipo = 1;
+
+    if(tipo === 1){
+        divObjetivo.style.backgroundColor = "#804000";
+    }
+    else{
+        divObjetivo.style.backgroundColor = "white";
+    }
+
+    setTimeout(() => {
+        // Regresar el círculo al principio
+        chocolate.style.transform = `translate(0, 0)`;
+    }, 2500)
+
 }
