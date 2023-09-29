34dc1269619f2b786d1f9e24a0631d9e0f726879
Beatriz <beatriz.moran@ikasle.egibide.org>
2023-09-29 13:25:26 +0200

    cambios css, modo automatico

--- pagina/script.js
+++ pagina/script.js
@@ -3,28 +3,88 @@ document.getElementById("claro").addEventListener("click", modoClaro);
 document.getElementById("oscuro").addEventListener("click", modoOscuro);
 document.getElementById("oscuro").style.backgroundColor = "#00ffcc";
 
-document.getElementById("reset").addEventListener("click", reset);
+document.getElementById("reset").addEventListener("click", resett);
+
+document.getElementById("automatico").addEventListener("click", opciones);
+document.getElementById("manual").addEventListener("click", opciones);
+
 
 document.addEventListener("DOMContentLoaded", function() {
-	
-
  setInterval(function() {
- fetch("posicion.html")
+ fetch("variables.html")
  .then(response => response.text())
  .then(data => {
- var pos = data.trim();
- console.log("Posición actual: " + pos);
+ var variables = data.trim().split("\n");
+ console.log("Variables recuperadas:", variables);
+
+ // Usar las variables almacenadas en el array
+ var martxa = variables[0].trim();
+ var resett = variables[1].trim();
+ var pos = variables[2].trim();
+ var contadorNegro = variables[3].trim();
+ var contadorBlanco = variables[4].trim();
+ var automatico = variables[5].trim();
+ var color = variables[6].trim();
+
  var posicion = "a" + pos;
 
- // Puedes utilizar la variable 'posicion' en otras partes de tu script aquí
  
- document.getElementById("marcha").addEventListener("click", moverElemento);
+ console.log("Posición actual: " + posicion);
+ console.log("Color: " + color);
+  console.log("martxa: " + martxa);
+ console.log("automatico: " + automatico);
+ 
+
+
+	 
+	 
+	     if( color === '2' || martxa === '0'){ //0 = false
+			document.getElementById("mensaje").textContent  = "Espere...";
+			document.getElementById("mensaje").style.color = "white";
+			console.log("no he salido");
+document.getElementById("mensaje").style.display = "block";
+
+    }
+	else{
+		if(automatico === '1'){ // 1 = true
+		document.getElementById("mensaje").style.display = "none";
+
+			console.log("he salido");
+			moverElemento();
+		}
+		else{
+			console.log("manual");
+			//cambiar
+			//moverElemento();
+		}
+	}
+	
+    
+    //AUTOMATICO Y MANUAL
+
+    document.getElementById("automatico").addEventListener("click", opciones);
+    document.getElementById("manual").addEventListener("click", opciones);
+
+    function opciones(e){
+        if(e.target.id = "automatico"){
+            automatico = '1';
+        }else{
+            if(e.target.id = "martxa"){
+                marcha = '1';
+            }else{
+            automatico = '0';
+            }
+        }
+    }
+
+
+ document.getElementById("marcha").addEventListener("click", opciones);
 
  
 function moverElemento() {
+	martxa = true;
     const chocolate = document.getElementById("chocolate");
     
-    alert(posicion)
     // Obtener el div objetivo
     const divObjetivo = document.querySelector(`.tabla-chocolates .${posicion}`);
   
@@ -40,28 +100,30 @@ function moverElemento() {
 
     //cambiar color fondo, 1 blanco 0 negro
 
-    tipo = 1;
 
-    if(tipo === 1){
+    if(color === '1'){
         divObjetivo.style.backgroundColor = "#804000";
         numChocoNegro=+1;
     }
     else{
         divObjetivo.style.backgroundColor = "white";
+		divObjetivo.style.color = "black";
+
         numChocoBlanco=+1;
     }
 
     setTimeout(() => {
         // Regresar el círculo al principio
         chocolate.style.transform = `translate(0, 0)`;
-    }, 2500)
+    }, 10000)
 }
  })
  .catch(error => {
  console.error("Error en la solicitud: ", error);
  });
- }, 1000);
+ }, 18000);
 });
+ 
 //var pos = JSON.stringify(posicion);
 
 
@@ -103,9 +165,11 @@ function modoOscuro(){
     document.body.style.color = "white"
 }
 
-function reset(){
+function resett(){
     location.reload();
 }
+
+
 
 
 
